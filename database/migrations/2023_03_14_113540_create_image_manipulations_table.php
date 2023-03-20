400e2f70e607385ca217de26c1dcd52ced854a11
Diego Granados <diego@ceicom.com.br>
2023-03-20 09:13:35 -0400

    first commit

---
+++ database/migrations/2023_03_14_113540_create_image_manipulations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('image_manipulations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('path', 2000);
+            $table->string('type', 25);
+            $table->text('data');
+            $table->string('output_path', 2000)->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->foreignIdFor(\App\Models\User::class, 'user_id')->nullable();
+            $table->foreignIdFor(\App\Models\Album::class, 'album_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('image_manipulations');
+    }
+};
